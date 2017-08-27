5e43c3f212b0a9a61b84d2b2f13bf78c58e27bee
quickSilverShanks <31253156+quickSilverShanks@users.noreply.github.com>
2017-08-27 23:20:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req,res) {
+   res.send("This is where my Article One will appear.");
+});
+app.get('/article-two', function(req,res) {
+   res.send("This is where my Article Two will appear.");
+});
+app.get('/article-three', function(req,res) {
+   res.send("This is where my Article Three will appear.");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
